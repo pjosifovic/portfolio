59dcced45519fcb512c749559ad5e93bbc407b4c
Predrag Josifovic <pjosifovic@yahoo.com>
2016-02-02 20:20:31 -0800

    adding event handler to main-nav element thats displaying Projects and About me as tabs

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -1,11 +1,15 @@
 var projectView = [];
 
 projectView.handleMainNav = function(){
+  $('.main-nav .tab').on('click', function(){
+    var $tabContent = $('.tab-content');
+    var $dataContent = $(this).attr('data-content');
+    $tabContent.hide();
+    $tabContent.filter('#' + $dataContent).show();
+  });
 
-
+  $('.main-nav .tab:first').click();
 };
-
-
 
 // Calling functions when DOM is ready
 $(function(){
