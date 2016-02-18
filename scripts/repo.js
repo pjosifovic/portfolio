9822906446b55523fdf0db905e9d5553da7011a6
Predrag Josifovic <pjosifovic@yahoo.com>
2016-02-17 16:23:50 -0800

    adding page and handlebars.js in vendors, changing Procfile just to run server

--- scripts/repo.js
+++ scripts/repo.js
@@ -3,11 +3,11 @@
 
   repos.all = [];
 
-  repos.requestRepos = function(cb) {
-    $.ajax('/github/users/pjosifovic/repos' + '?per_page=4&sort=updated',
-      function(data, message, xhr) {
-        repos.all = data;
-      }).done(cb);
+  repos.requestRepos = function(callback) {
+    $.get('/github/users/pjosifovic/repos', function(data, message, xhr) {
+      console.log('in the repo');
+      repos.all = data;
+    }).done(callback);
   };
 
   repos.with = function(attr) {
